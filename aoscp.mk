bf45ce3d053d22d997275895e95d9e98fd8dd4e4
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-08-24 17:46:15 +0200

    hero2lte: adapt device tree to build aoscp
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- aoscp.mk
+++ aoscp.mk
@@ -8,11 +8,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit AOSCP common bits
+$(call inherit-product, vendor/aoscp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aoscp/configs/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := aoscp_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G935F
