21ef55e5334b1ab198ae3254924222634c0f2273
Yuv <dailytroll92@gmail.com>
2017-09-01 15:29:29 -0600

    Revert "Adapted to DU"
    This reverts commit 2e1ca9bc2bcd2cfd572547155ffc7acec9800b1a.

--- aoscp.mk
+++ aoscp.mk
@@ -6,16 +6,18 @@ $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit AOSCP common bits
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aoscp/configs/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := du_hero2lte
+PRODUCT_NAME := aoscp_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G935F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-TARGET_VENDOR := samsung
